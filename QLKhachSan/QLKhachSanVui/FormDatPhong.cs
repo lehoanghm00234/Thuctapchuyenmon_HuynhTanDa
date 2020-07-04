f324f90ef953f0a5a869b4beb29da8c55941d871
unknown <lehoanghm00234@gmail.com>
2020-07-04 22:32:30 +0700

    Quan lý khách sạn

---
+++ QLKhachSan/QLKhachSanVui/FormDatPhong.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Collections;
+
+namespace QLKhachSanVui
+{
+
+    public partial class FormDatPhong : Form
+    {
+        
+
+        public FormDatPhong()
+        {
+            InitializeComponent();
+        }
+
+        private void FormDatPhong_Load(object sender, EventArgs e)
+        {
+            
+        }
+        private void LoadComboKH()
+        {
+        }
+        private void LoadDataKh()
+        {
+           
+        }
+        private void TimPhongTrong()
+        {
+            
+        }
+        private void btnTim_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void cbkh_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+        public void LoadCTKH()
+        {
+            
+        }
+
+        private void btnDatphong_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void txtSonguoi_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void txtTiencoc_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void tabControl1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void ChonPhong()
+        {
+           
+        }
+
+        private void gvTimPhong_SelectionChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ QLKhachSan/QLKhachSanVui/FormPhong.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace QLKhachSanVui
+{
+    public partial class FormPhong : Form
+    {
+        public FormMain frmMain;
+
+        DataClasses1DataContext dt = new DataClasses1DataContext();
+        public FormPhong()
+        {
+            InitializeComponent();
+        }
+
+        private void FormPhong_Load(object sender, EventArgs e)
+        {
+            dataGridView1.AutoGenerateColumns = false;
+            dataGridView1.DataSource = dt.phongs.ToList();
+            cbLoaiPhong.DataSource = dt.loaiphongs.ToList();
+            cbLoaiPhong.DisplayMember = "maloai";
+            cbLoaiPhong.ValueMember = "maloai";
+
+
+            cbMatinhtrang.DataSource = dt.tinhtrangs.ToList();
+            cbMatinhtrang.DisplayMember = "matinhtrang";
+            cbMatinhtrang.ValueMember = "matinhtrang";
+
+            
+        }
+
+        private void LoadDataPhong()
+        {
+            
+        }
+        private void LoadComboLoai()
+        {
+            
+            //cbLoaiPhong.ValueMember = "maloai";
+
+        }
+        private void ReadDataPhong()
+        {
+            
+        }
+        private void LoadDataLoaiPhong()
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+           
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+           
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void tabControl1_Click(object sender, EventArgs e)
+        {
+           // panel1.Enabled = true;
+        }
+        int i = 0;
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+
+            panel1.Visible = false;
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            dt.themPhong(txtMaphong.Text, cbMatinhtrang.SelectedValue.ToString(), cbLoaiPhong.SelectedValue.ToString());
+            FormPhong_Load(sender, e);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
